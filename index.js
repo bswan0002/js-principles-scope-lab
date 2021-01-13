e944e7709c0ba5ad79ae5be354e650233a060648
bswan0002 <bswan0002@gmail.com>
2021-01-13 14:41:03 -0800

    passing

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "someVal";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = "not bob";
+  return;
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "what's up";
+}
